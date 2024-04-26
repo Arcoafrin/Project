eb64949d0a965a7aa108282f471353ad3826c4ba
Arcoafrin <161829930+Arcoafrin@users.noreply.github.com>
2024-04-26 14:20:19 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,15 +4,21 @@ import ReactDOM from "react-dom";
 const date = new Date();
 const currentTime = date.getHours();
 let greeting;
+const customStyle = {
+  color: ""
+}
 if (currentTime < 12) {
   greeting = "Good morning";
+  customStyle.color = "red";
 } else if (currentTime < 18) {
   greeting = "Good afternoon";
+  customStyle.color = "green";
 } else {
   greeting = "Good night";
+  customStyle.color = "blue";
 }
 ReactDOM.render(
-  <h1 className="heading" contentEditable>
+  <h1 className="heading" style={customStyle}contentEditable>
     {greeting}
   </h1>,
   document.getElementById("root")
